be0ec736d42ab63a3fd523204bbfec26e46737c0
Bryce David Pearson <brycedavidpearson@gmail.com>
2026-08-16 21:10:21 +0800

    Add private file attachments to evaluation records

--- app/evaluations/[id]/page.tsx
+++ app/evaluations/[id]/page.tsx
@@ -1,8 +1,9 @@
+import { AttachmentPanel } from "@/components/AttachmentPanel";
 import { FollowUpPanel } from "@/components/FollowUpPanel";
 import { LessonPlanExportPanel } from "@/components/LessonPlanExportPanel";
 import { ProductionEvaluationRecord } from "@/components/ProductionEvaluationRecord";
 
 export default async function EvaluationPage({params}:{params:Promise<{id:string}>}) {
   const {id}=await params;
-  return <><ProductionEvaluationRecord evaluationId={id}/><LessonPlanExportPanel evaluationId={id}/><FollowUpPanel evaluationId={id}/></>;
+  return <><ProductionEvaluationRecord evaluationId={id}/><AttachmentPanel evaluationId={id}/><LessonPlanExportPanel evaluationId={id}/><FollowUpPanel evaluationId={id}/></>;
 }
