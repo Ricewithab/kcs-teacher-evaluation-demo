a153145e4187a2b3810a716467843289a7523904
Bryce David Pearson <brycedavidpearson@gmail.com>
2026-08-16 21:10:54 +0800

    Load attachment styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "./production.css";
 import "./production-workflow.css";
 import "./master-admin.css";
 import "./print.css";
+import "./attachments.css";
 
 export const metadata = {
   title: "KCS Teacher Evaluation",
