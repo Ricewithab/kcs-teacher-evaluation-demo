6a49a49b61f1f817f02a7684feb9b62db4a5c9ec
Bryce David Pearson <brycedavidpearson@gmail.com>
2026-08-16 20:42:58 +0800

    Add first-run setup page

---
+++ app/setup/page.tsx
@@ -0,0 +1,7 @@
+import { SetupForm } from "@/components/SetupForm";
+
+export const metadata = { title: "Initial setup · KCS Teacher Evaluation" };
+
+export default function SetupPage() {
+  return <SetupForm/>;
+}
