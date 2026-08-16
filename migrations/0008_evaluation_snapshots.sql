9324a088394dbeee7c327969fd2ab4d88f18f57f
Bryce David Pearson <brycedavidpearson@gmail.com>
2026-08-16 21:18:07 +0800

    Snapshot evaluation type and rubric at scheduling

---
+++ migrations/0008_evaluation_snapshots.sql
@@ -0,0 +1,3 @@
+ALTER TABLE evaluations ADD COLUMN evaluation_type TEXT;
+ALTER TABLE evaluations ADD COLUMN rubric_snapshot_json TEXT;
+ALTER TABLE evaluations ADD COLUMN rating_scale_snapshot_json TEXT;
