006e5a13b096a5ea1ca75ecc8e83013d48400213
Bryce David Pearson <brycedavidpearson@gmail.com>
2026-08-16 21:03:37 +0800

    Link production administration from Master Management

--- app/master/page.tsx
+++ app/master/page.tsx
@@ -1,7 +1,8 @@
 import { CycleSyncPanel } from "@/components/CycleSyncPanel";
+import { MasterAdminLinks } from "@/components/MasterAdminLinks";
 import { MasterConfiguration } from "@/components/MasterConfiguration";
 import { RubricConfiguration } from "@/components/RubricConfiguration";
 
 export default function MasterPage() {
-  return <><MasterConfiguration/><RubricConfiguration/><CycleSyncPanel/></>;
+  return <><MasterAdminLinks/><MasterConfiguration/><RubricConfiguration/><CycleSyncPanel/></>;
 }
