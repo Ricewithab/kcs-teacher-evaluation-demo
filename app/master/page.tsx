37e97c20a79dcdac5320ba747e54e5642a2a21e2
Bryce David Pearson <brycedavidpearson@gmail.com>
2026-08-16 21:17:04 +0800

    Use mode-specific Master workspace

--- app/master/page.tsx
+++ app/master/page.tsx
@@ -1,8 +1,5 @@
-import { CycleSyncPanel } from "@/components/CycleSyncPanel";
-import { MasterAdminLinks } from "@/components/MasterAdminLinks";
-import { MasterConfiguration } from "@/components/MasterConfiguration";
-import { RubricConfiguration } from "@/components/RubricConfiguration";
+import { MasterWorkspace } from "@/components/MasterWorkspace";
 
 export default function MasterPage() {
-  return <><MasterAdminLinks/><MasterConfiguration/><RubricConfiguration/><CycleSyncPanel/></>;
+  return <MasterWorkspace/>;
 }
