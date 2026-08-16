3a989f2323b0c3b464779ef9937392accad196c8
Bryce David Pearson <brycedavidpearson@gmail.com>
2026-08-16 21:07:00 +0800

    Add previous-target follow-up to evaluation records

--- app/evaluations/[id]/page.tsx
+++ app/evaluations/[id]/page.tsx
@@ -1,6 +1,7 @@
+import { FollowUpPanel } from "@/components/FollowUpPanel";
 import { ProductionEvaluationRecord } from "@/components/ProductionEvaluationRecord";
 
 export default async function EvaluationPage({params}:{params:Promise<{id:string}>}) {
   const {id}=await params;
-  return <ProductionEvaluationRecord evaluationId={id}/>;
+  return <><ProductionEvaluationRecord evaluationId={id}/><FollowUpPanel evaluationId={id}/></>;
 }
