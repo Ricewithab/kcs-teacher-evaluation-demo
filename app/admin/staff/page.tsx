dae62722c915118037558cfdcad523d760ab9b3c
Bryce David Pearson <brycedavidpearson@gmail.com>
2026-08-16 21:02:29 +0800

    Add whole-school staff management page

---
+++ app/admin/staff/page.tsx
@@ -0,0 +1,5 @@
+import { StaffManagement } from "@/components/StaffManagement";
+
+export const metadata = { title: "Staff & organisation · KCS Teacher Evaluation" };
+
+export default function StaffPage(){return <StaffManagement/>}
