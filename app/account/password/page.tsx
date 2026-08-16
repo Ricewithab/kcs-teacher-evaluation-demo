46aa24428777aa6854b643beb7db08b4fbaadf48
Bryce David Pearson <brycedavidpearson@gmail.com>
2026-08-16 20:43:16 +0800

    Add password settings page

---
+++ app/account/password/page.tsx
@@ -0,0 +1,7 @@
+import { PasswordChangeForm } from "@/components/PasswordChangeForm";
+
+export const metadata = { title: "Password · KCS Teacher Evaluation" };
+
+export default function PasswordPage() {
+  return <PasswordChangeForm/>;
+}
