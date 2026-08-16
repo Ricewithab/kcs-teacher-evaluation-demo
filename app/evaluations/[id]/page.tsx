6a4686df8de5218adfbd18992dc768421a2fc4ad
Bryce David Pearson <brycedavidpearson@gmail.com>
2026-08-16 21:23:18 +0800

    Add reschedule and cancellation management

--- app/evaluations/[id]/page.tsx
+++ app/evaluations/[id]/page.tsx
@@ -1,9 +1,10 @@
 import { AttachmentPanel } from "@/components/AttachmentPanel";
+import { EvaluationScheduleActions } from "@/components/EvaluationScheduleActions";
 import { FollowUpPanel } from "@/components/FollowUpPanel";
 import { LessonPlanExportPanel } from "@/components/LessonPlanExportPanel";
 import { ProductionEvaluationRecord } from "@/components/ProductionEvaluationRecord";
 
 export default async function EvaluationPage({params}:{params:Promise<{id:string}>}) {
   const {id}=await params;
-  return <><ProductionEvaluationRecord evaluationId={id}/><AttachmentPanel evaluationId={id}/><LessonPlanExportPanel evaluationId={id}/><FollowUpPanel evaluationId={id}/></>;
+  return <><ProductionEvaluationRecord evaluationId={id}/><EvaluationScheduleActions evaluationId={id}/><AttachmentPanel evaluationId={id}/><LessonPlanExportPanel evaluationId={id}/><FollowUpPanel evaluationId={id}/></>;
 }
